e704a148cf526c9b758834ef2ebbdc7d12a2860b
luholiveira56 <lu56oliveira@gmail.com>
2024-09-13 00:36:55 -0300

    fundamentos 004

--- aula.md
+++ aula.md
@@ -7,7 +7,7 @@ Como um lego, você irá utilizar peças para criar algoritmos, ou seja, para re
 
 ## Peças de uma linguagem
 
-- [x] Comentários
+- Comentários
 - Declaração de variáveis (const, let)
 - Operadores (atribuição, concatenação, matemáticos, lógicos )
 - Tipos de dados (string, number, boolean)
@@ -55,3 +55,7 @@ Apresentar os dados
 - [x] criar, passar argumento
 - [x] excutar
 - [x] arrow function / named function
+
+# Estrutura de repetição
+
+- [x] while
--- index.js
+++ index.js
@@ -1,53 +1,64 @@
-// olá, mundo!
-console.log("olá, mundo!")
+// // olá, mundo!
+// console.log("olá, mundo!")
 
 
-// variáveis - let
-let mensagens = "olá, mundo!"
+// // variáveis - let
+// let mensagens = "olá, mundo!"
 
 
-// variáveis - const
-const mensagem = "olá, eu"
-{
-    const mensagem = "olá, Mayk!"
-    console.log(mensagem)
-}
-console.log(mensagem);
+// // variáveis - const
+// const mensagem = "olá, eu"
+// {
+//     const mensagem = "olá, Mayk!"
+//     console.log(mensagem)
+// }
+// console.log(mensagem);
 
 
-// arrays
-let metas = ['mayk', 'alô']
-let metas01 = [2, 'mayk']
-// concatenando valores
-console.log(metas[1] + ", " + metas[0]) 
+// // arrays
+// let metas = ['mayk', 'alô']
+// let metas01 = [2, 'mayk']
+// // concatenando valores
+// console.log(metas[1] + ", " + metas[0]) 
 
-let metas03 = {
-    value: 'ler um livro por mês',
-    checked: true,
-}
+// let metas03 = {
+//     value: 'ler um livro por mês',
+//     checked: true,
+// }
 
-let metas02 = [
-    metas03,
-    {
-        value: 'caminhar 20 minutos todos os dias',
-        checked: false
+// let metas02 = [
+//     metas03,
+//     {
+//         value: 'caminhar 20 minutos todos os dias',
+//         checked: false
+//     }
+// ]
+// console.log(metas02[1].value);
+
+// // objetos
+// let meta = {
+//     value: 'ler um livro por mês',
+//     address: 2,
+//     checked: true,
+//     isChecked: () => {
+//         console.log(info)
+//     }
+// }
+// console.log(meta.value);
+
+// // function // arrow function
+// const criarMeta = () => {}
+
+// // named function
+// function criarMetas() {}
+
+// while
+const start = () => {
+    let count = 0
+    while(count <= 10){
+        console.log(count)
+        count = count + 1
     }
-]
-console.log(metas02[1].value);
-
-// objetos
-let meta = {
-    value: 'ler um livro por mês',
-    address: 2,
-    checked: true,
-    isChecked: () => {
-        console.log(info)
-    }
 }
-console.log(meta.value);
 
-// function // arrow function
-const criarMeta = () => {}
-
-// named function
-function criarMetas() {}
+start()
